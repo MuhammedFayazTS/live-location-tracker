5994d3e3e438950ef16859f76de6cbb241f09204
MUHAMMED FAYAZ T S <muhammedfayazts01@gmail.com>
2024-07-11 12:51:14 +0530

    feat: add map and location of user
    - real time connection using socket io
    - user current location as a marker

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import http from "http";
 import { Server } from "socket.io";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
@@ -8,8 +10,19 @@ const server = http.createServer(app);
 
 const io = new Server(server);
 
+app.set("view engine", "ejs");
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+  socket.on("send-location", (data) => {
+    io.emit("recieve-location", { id: socket._id, ...data });
+  });
+  console.log("connected");
+});
+
 app.get("/", (req, res) => {
-  res.send("Server is Running");
+  res.render("index");
 });
 
 export { server };
